67969b7c134496d5b40adf7cfdf2bb8133e051b7
wenchong <18256786+wenchonglee@users.noreply.github.com>
2022-08-02 23:54:01 +0800

    removed robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: process.env.SITE_URL || "https://wenchonglee.dev",
-  generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
 
