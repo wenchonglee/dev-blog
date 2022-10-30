4f809e69a9a4f754aecb2ded57beea9c39816568
wenchong <18256786+wenchonglee@users.noreply.github.com>
2022-10-30 15:25:18 +0800

    Tweaked sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://wenchonglee.dev",
+  siteUrl: process.env.SITE_URL || "https://www.wenchonglee.dev",
   generateIndexSitemap: false,
 };
 
