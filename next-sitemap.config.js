52e5e5c471f3563ec2535458ceb7656cb51b1da6
wenchonglee <18256786+wenchonglee@users.noreply.github.com>
2023-09-06 23:38:51 +0800

    changed setup to astro v3

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: process.env.SITE_URL || "https://www.wenchonglee.dev",
-  generateIndexSitemap: false,
-};
-
-export default config;
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "wenchonglee.dev";
+export const SITE_DESCRIPTION = "Wen Chong's blog. Software Engineer based in Singapore";
