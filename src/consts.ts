cbccd81cd56c7a56e73ef784db931c6420fe98ff
wenchonglee <18256786+wenchonglee@users.noreply.github.com>
2026-05-03 15:07:15 +0800

    nuke repo

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "wenchonglee.dev";
-export const SITE_DESCRIPTION = "Wen Chong's blog. Software Engineer based in Singapore";
